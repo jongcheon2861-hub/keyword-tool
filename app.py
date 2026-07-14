e29c524d80bd2208044a1f565076681c781610a3
jongcheon2861-hub <jongcheon2861@gmail.com>
2026-07-14 14:08:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1201,7 +1201,7 @@ with st.sidebar:
         favicon = icon if icon else ("https://www.google.com/s2/favicons?domain=" + url + "&sz=32")
         st.markdown(
             f"<a href='{url}' target='_blank' rel='noopener noreferrer' class='side-link' "
-            f"onclick=\"window.open('{url}','_blank');return false;\">"
+            f"onclick=\"window.top.open('{url}','_blank');return false;\">"
             f"<img src='{favicon}' class='side-ico'>"
             f"<span>{name}</span></a>",
             unsafe_allow_html=True)
