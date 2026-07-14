51ff0766422590b3491aff0fcfa553c514d004e6
jongcheon2861-hub <jongcheon2861@gmail.com>
2026-07-14 14:10:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1200,8 +1200,10 @@ with st.sidebar:
     for name, url, icon in links:
         favicon = icon if icon else ("https://www.google.com/s2/favicons?domain=" + url + "&sz=32")
         st.markdown(
-            f"[![]({favicon}) {name}]({url})",
-            unsafe_allow_html=False)
+            f"<a href='{url}' target='_blank' rel='noopener noreferrer' class='side-link'>"
+            f"<img src='{favicon}' class='side-ico'>"
+            f"<span>{name}</span></a>",
+            unsafe_allow_html=True)
 
 if menu == "🧮 마진계산":
     render_margin_calculator()
